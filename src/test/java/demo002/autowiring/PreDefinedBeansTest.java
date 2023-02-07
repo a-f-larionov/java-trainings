9bb0400258e60d0d68564bae86b09676e870ab15
aleksandr.larionov <a.f.larionov@gmail.com>
2023-02-07 15:56:41 +0300

    add pre defined beans test

---
+++ src/test/java/demo002/autowiring/PreDefinedBeansTest.java
@@ -0,0 +1,29 @@
+package demo002.autowiring;
+
+import demo002.autowiring.predefined_beans.BeanWithPredefined;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PreDefinedBeansTest {
+
+    @Test
+    public void applicationContext() {
+        // given
+        var cnxt = new AnnotationConfigApplicationContext();
+        cnxt.register(BeanWithPredefined.class);
+        cnxt.refresh();
+
+        // when
+        var bean = cnxt.getBean("bean", BeanWithPredefined.class);
+
+        // then
+        assertThat(bean.getApplicationContext()).isSameAs(cnxt);
+        assertThat(bean.getEnvironment()).isInstanceOf(Environment.class);
+        assertThat(bean.getBeanFactory()).isInstanceOf(BeanFactory.class);
+        // assertThat(bean.getLifecycleProcessor()).isInstanceOf(LifecycleProcessor.class);
+    }
+}
